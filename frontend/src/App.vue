bd207540c7d56aac018e71a982a0e4f366b47887
skarala <info@furbo.sk>
2020-04-15 14:48:03 +0200

    updates design of buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,13 @@
   <div id="app">
     <div class="container-fluid">
       <h1 class="text-center title">Spelleology</h1>
+      <div class="col-md-6 mx-auto">
+        <input type="text" placeholder="search for spell effect" v-model="search" class="search" />
+        <button v-on:click="clearAll" class="custom-underline fancy-button">Delete all</button>
+        <button v-on:click="reset" class="custom-underline">Reset</button>
+      </div>
       <div class="row">
-        <div class="col col-md-8 offset-md-1">
+        <div class="col col-md-12 mx-auto">
           <SpellList
             v-if="spells.length >=1"
             :spells="filteredList"
@@ -12,17 +17,7 @@
           />
           <h1 class="subtitle" v-else>Mischief managed</h1>
         </div>
-        <div class="col col-md-3 sticky-top">
-          <button v-on:click="clearAll">Clear</button>
-          <button v-on:click="reset">Reset</button>
-
-          <input
-            type="text"
-            placeholder="search for spell effect"
-            v-model="search"
-            class="form-control search"
-          />
-        </div>
+        <div class="col col-md-3 sticky-top"></div>
       </div>
     </div>
   </div>
@@ -91,23 +86,19 @@ h1.subtitle {
   font-family: "Harry", serif;
   margin: 100px;
   font-size: 6em;
-  color: white;
-  text-shadow: 7px 7px 2px #671e1c;
+  color: #dca39e;
 }
 
 button {
+  padding: 1px 20px;
   background-color: #822724;
   border: none;
-  font-family: "Lora", serif;
+  font-family: "Kanit", serif;
   color: #fffcf9;
-  font-size: 1.5em;
-  margin: 30px;
-  padding: 10px 30px;
+  font-size: 2em;
   cursor: pointer;
-  border-radius: 3px;
-  -webkit-box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 1);
-  -moz-box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 1);
-  box-shadow: 0px 0px 17px -4px rgba(0, 0, 0, 1);
+  border-bottom: 2px solid #dca39e;
+  margin: 0 20px;
 }
 button:focus {
   outline: none;
@@ -118,5 +109,42 @@ div.controller {
 @font-face {
   font-family: "Harry";
   src: url("./assets/HARRYP.TTF");
+}
+input.search {
+  outline: none;
+  border: none;
+  color: white;
+  font-family: "Lora", serif;
+  border-bottom: 2px solid #dca39e;
+  background-color: #822724;
+  border-radius: 0;
+  font-size: 2em;
+  margin-bottom: 50px;
+  transition: all 200ms ease;
+}
+input.search:focus,
+input.search:active {
+  outline: none;
+  border: none;
+  box-shadow: none;
+  background-color: #822724;
+  border-radius: 0;
+  border-bottom: 1px solid white;
+  color: white;
+}
+
+button:hover {
+  border-bottom: 2px solid white;
+  color: white;
+}
+input.search::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #dca39e;
+  font-family: "Kanit", serif;
+}
+input.search:focus::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: white;
+  font-family: "Kanit", serif;
 }
 </style>
