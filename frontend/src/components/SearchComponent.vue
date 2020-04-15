729802b897cc839c0471d8047d0c49ecff003dd7
skarala <info@furbo.sk>
2020-04-15 15:13:14 +0200

    separates Search component

---
+++ frontend/src/components/SearchComponent.vue
@@ -0,0 +1,76 @@
+
+<template>
+  <input
+    type="text"
+    class="search"
+    placeholder="search for spell effect"
+    v-bind="$attrs"
+    v-bind:value="value"
+    v-on="inputListeners"
+  />
+</template>
+
+<script>
+export default {
+  name: "SearchComponent",
+  props: ["value"],
+  computed: {
+    inputListeners: function() {
+      var vm = this;
+      // `Object.assign` merges objects together to form a new object
+      return Object.assign(
+        {},
+        // We add all the listeners from the parent
+        this.$listeners,
+        // Then we can add custom listeners or override the
+        // behavior of some listeners.
+        {
+          // This ensures that the component works with v-model
+          input: function(event) {
+            vm.$emit("input", event.target.value);
+          }
+        }
+      );
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+input.search {
+  outline: none;
+  border: none;
+  color: white;
+  font-family: "Lora", serif;
+  border-bottom: 2px solid #dca39e;
+  background-color: #822724;
+  border-radius: 0;
+  font-size: 2em;
+  margin-bottom: 50px;
+  transition: all 200ms ease;
+}
+
+input.search:focus,
+input.search:active {
+  outline: none;
+  border: none;
+  box-shadow: none;
+  background-color: #822724;
+  border-radius: 0;
+  border-bottom: 1px solid white;
+  color: white;
+}
+
+input.search::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #dca39e;
+  font-family: "Kanit", serif;
+}
+input.search:focus::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: white;
+  font-family: "Kanit", serif;
+}
+</style>
