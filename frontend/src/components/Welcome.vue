acaec41c46c8fd2eeaad1b04d696d9d445e332e2
skarala <info@furbo.sk>
2020-04-23 12:36:23 +0200

    magnificent design changes

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,6 +1,6 @@
  <template>
-  <div>
-    <h1>Welcome Wizard</h1>
+  <div class="welcome">
+    <h1 class="welcome-title">Howdy Wizard</h1>
   </div>
 </template>
 
@@ -9,4 +9,11 @@ export default {
   name: "Welcome"
 };
 </script>
+<style >
+h1.welcome-title {
+  margin-top: 200px;
+  font-size: 8em;
+  color: rgb(189, 189, 189);
+}
+</style>
 
