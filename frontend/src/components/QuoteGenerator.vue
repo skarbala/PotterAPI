b964ccf4c41bf20d08ab312b27cef584e1864e33
skarala <info@furbo.sk>
2020-05-13 17:02:37 +0200

    updated design of Quotegenerator

--- frontend/src/components/QuoteGenerator.vue
+++ frontend/src/components/QuoteGenerator.vue
@@ -1,45 +1,41 @@
 <template>
   <div class="main">
     <h1 class="title">Potter Quotes</h1>
+    <h2 class="subtitle">Level up by getting some wisdom</h2>
+
     <div class="quote-controls">
       <button v-on:click="getQuote" class="main">Get Quote</button>
-      <div class="wisdom-level">
-        <div class="row">
-          <div class="col">
-            <p>
-              <span>wisdom points</span>
-              +{{this.quoteList.length}}
-            </p>
-          </div>
+      <div class="row wisdom-level col-md-8 mx-auto">
+        <div class="col">
+          <p>
+            <span>wisdom points</span>
+            +{{this.quoteList.length}}
+          </p>
         </div>
-        <div class="row">
-          <div class="col">
-            <p>
-              <span>current level</span>
-              {{wizardLevel}}
-            </p>
-          </div>
+        <div class="col">
+          <p>
+            <span>current level</span>
+            {{wizardLevel}}
+          </p>
         </div>
       </div>
     </div>
 
     <div class="col-md-6 mx-auto">
       <div class="quote-container">
-        <div v-if="quote" class="quote">
-          <h1>"{{quote.quote}}"</h1>
-          <h2>{{quote.author}}</h2>
-        </div>
-        <div v-else>Click button to get some wisdom</div>
+        <div
+          v-if="this.qouteCounter ==0"
+          class="empty-list-message"
+        >Click the button to get some wisdom</div>
+        <transition-group v-else name="fade" tag="ul" class="recent-quotes">
+          <li v-for="quoteitem in quoteList" :key="quoteitem.quote">
+            <div>
+              <p>{{ quoteitem.quote }}</p>
+              <p class="author">{{ quoteitem.author }}</p>
+            </div>
+          </li>
+        </transition-group>
       </div>
-
-      <transition-group name="fade" tag="ul" class="recent-quotes">
-        <li v-for="quoteitem in quoteList" :key="quoteitem.quote">
-          <div>
-            <p>{{ quoteitem.quote }}</p>
-            <p>{{ quoteitem.author }}</p>
-          </div>
-        </li>
-      </transition-group>
     </div>
   </div>
 </template>  
@@ -106,32 +102,35 @@ export default {
 h1.title {
   font-family: "Roboto Slab", serif;
 }
+h2.subtitle {
+  font-family: "Roboto Slab", serif;
+  color: rgb(189, 189, 189);
+  font-size: 1.5em;
+}
 
 div.quote-container {
-  min-height: 200px;
-  margin-bottom: 50px;
+  margin-top: 50px;
 }
-div.quote h1 {
-  text-align: left;
-  color: rgb(92, 92, 92);
-}
-div.quote h2 {
-  text-align: left;
-  color: rgb(189, 189, 189);
-}
+
 button {
-  background-color: rgb(189, 189, 189);
   border: none;
   cursor: pointer;
   padding: 10px 30px;
-  margin-top: 50px;
+  margin-top: 20px;
   background-color: #822724;
 }
-
 ul.recent-quotes {
   list-style: none;
 }
+ul.recent-quotes li:nth-child(1) {
+  transform: scale(1.05);
+  margin-bottom: 20px;
+  -webkit-box-shadow: 0px 0px 7px 1px #822724;
+  -moz-box-shadow: 0px 0px 7px 1px #822724;
+  box-shadow: 0px 0px 15px 3px #c17f7d;
+}
 ul.recent-quotes div {
+  font-family: "Roboto Slab", serif;
   text-align: left;
   padding: 20px;
   margin-bottom: 10px;
@@ -139,13 +138,17 @@ ul.recent-quotes div {
   -moz-box-shadow: 0px 0px 7px 1px rgba(230, 230, 230, 1);
   box-shadow: 0px 0px 7px 1px rgba(230, 230, 230, 1);
 }
-.fade-enter-active,
+ul.recent-quotes p.author {
+  color: rgb(189, 189, 189);
+}
+.fade-enter-to,
 .fade-leave-active {
-  transition: opacity 1s;
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
+
 div.quote-controls p {
   display: inline-block;
   color: #822724;
@@ -155,6 +158,11 @@ div.quote-controls p {
 }
 div.quote-controls button:active {
   transform: scale(1.1);
+}
+.empty-list-message {
+  color: rgb(189, 189, 189);
+  font-size: 2em;
+  font-family: "Roboto Slab", serif;
 }
 
 div.quote-controls button {
@@ -166,5 +174,23 @@ div.wisdom-level p span {
 }
 div.wisdom-level {
   margin-top: 20px;
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
