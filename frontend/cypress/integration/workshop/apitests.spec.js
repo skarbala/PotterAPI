645dfeb02e0315084aeb7bd38ec7aa122990ad32
martin <skarbala.martin@gmail.com>
2020-07-14 08:40:47 +0200

    Sorting hat: add tests to check response and extract response body

--- frontend/cypress/integration/workshop/apitests.spec.js
+++ frontend/cypress/integration/workshop/apitests.spec.js
@@ -30,7 +30,28 @@ describe("api test example", () => {
     cy.request('DELETE', 'http://localhost:3000/spells/actions/deleteAll')
     cy.reload()
     cy.contains("sneezing forever").should("not.be.visible")
-
   })
+
+
+  it('should fetch all spells via api', () => {
+    cy.server();
+    cy.request(Cypress.env('apiUrl') + '/sortingHat')
+      .then(response => {
+        expect(response.status).to.eql(200)
+        cy.log(response.body.house)
+      });
+  });
+
+  it.only('should catch response and extract body and check value on page', () => {
+    cy.server();
+    cy.route(Cypress.env('apiUrl') + '/sortingHat').as('sortingHat');
+
+    cy.visit("http://localhost:8080/#/sortingHat")
+    cy.get('button.main').click();
+    cy.wait('@sortingHat').then(
+      response => cy.get('.col-md-4 > h1').should('have.text', response.responseBody.house)
+    )
+  })
+
 })
 
