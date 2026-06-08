ac0db715fdaa4d9984289c2f9c1ae430be7edae6
martinskarbala <martin@supernova.io>
2026-06-08 14:37:05 +0200

    refactor: update axios base URL and standardize import formatting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,33 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import axios from "axios";
-import VueRouter from 'vue-router'
-import Spelleology from '../src/components/Spelleology'
-import SortingHat from '../src/components/SortingHat'
-import Welcome from '../src/components/Welcome'
-import QuoteGenerator from '../src/components/QuoteGenerator'
-import GringottsBank from '../src/components/GringottsBank'
+import VueRouter from "vue-router";
+import Spelleology from "../src/components/Spelleology";
+import SortingHat from "../src/components/SortingHat";
+import Welcome from "../src/components/Welcome";
+import QuoteGenerator from "../src/components/QuoteGenerator";
+import GringottsBank from "../src/components/GringottsBank";
 
 const axiosconfig = {
-  baseURL: 'http://localhost:3000/',
+  baseURL: "https://potterapi-tgq2.onrender.com",
   timeout: 30000,
 };
-Vue.prototype.$http = axios.create(axiosconfig)
-Vue.use(VueRouter)
-
+Vue.prototype.$http = axios.create(axiosconfig);
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
-    { path: '/spelleology', component: Spelleology },
-    { path: '/sortingHat', component: SortingHat },
-    { path: '/quotes', component: QuoteGenerator },
-    { path: '/gringottsBank', component: GringottsBank },
-    { path: '/', component: Welcome }
-
-  ]
+    { path: "/spelleology", component: Spelleology },
+    { path: "/sortingHat", component: SortingHat },
+    { path: "/quotes", component: QuoteGenerator },
+    { path: "/gringottsBank", component: GringottsBank },
+    { path: "/", component: Welcome },
+  ],
 });
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
